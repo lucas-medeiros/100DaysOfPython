b0ffb1c5bd4524725266ca86e7b83160a7812782
Lucas Cardoso de Medeiros <lucascarmed@gmail.com>
2022-08-18 08:41:04 -0300

    Day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,6 @@
+# Generate band name
+if __name__ == '__main__':
+    print("Welcome to the Band Name Generator.")
+    city = input("What's name of the city you grew up?\n")
+    pet = input("What's your name?\n")
+    print(f"Your band name could be {city} {pet}")
